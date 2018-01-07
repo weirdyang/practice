bb595e6ac80f0e38822d8b1fd7e6c0c82b02e902
captmomo <wy4786@gmail.com>
2018-01-07 23:51:59 +0800

    Update generator.py

--- text_generator/generator.py
+++ text_generator/generator.py
@@ -5,7 +5,7 @@ from nltk import word_tokenize
 from nltk.collocations import TrigramAssocMeasures
 from nltk.util import ngrams
 
-def generate_model(list_ngrams, word, num=100):
+def generate_model(list_ngrams, word, num=90):
     """http://www.cyber-omelette.com/2017/01/markov.html
     generate model based on ngrams
     """
@@ -15,9 +15,9 @@ def generate_model(list_ngrams, word, num=100):
     while len(message.split(' ')) < num:
         random.shuffle(list_ngrams)
         for item in list_ngrams:
-            if item[0] == word:
-                message += "{0} {1} {2} ".format(item[1], item[2], item[3])
-                word = CFD[item[3]].max()
+            if item[0] == word.lower():
+                message += "{0} {1} ".format(item[1], item[2])
+                word = CFD[item[2]].max().lower()
                 list_random.remove(item)
     return message
 
@@ -37,8 +37,8 @@ with open('letsrun_1.txt', mode='r', encoding='utf-8') as f:
 
 words = word_tokenize(raw.lower())
 print(words)
-generated_ngrams = list(ngrams(words, 4))
-#bigrams = nltk.bigrams(words)
+generated_ngrams = list(ngrams(words, 3))
+bigrams = nltk.bigrams(words)
 CFD = nltk.ConditionalFreqDist(bigrams)
 FD = nltk.FreqDist(generated_ngrams)
 print(FD)
