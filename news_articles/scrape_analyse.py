3047ef2ed617910382c98945752a213936531fc0
captmomo <wy4786@gmail.com>
2018-01-07 14:17:47 +0800

    Update scrape_analyse.py

--- news_articles/scrape_analyse.py
+++ news_articles/scrape_analyse.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from analyzer import Analyzer
 
 
+
 Base_url = "http://www.moneycontrol.com"
 
 # Build a dictionary of companies and their abbreviated names
@@ -18,10 +19,10 @@ companies = { 'cadilahealthcare': 'CHC', 'piramalenterprises': 'PH05',
 master_list =[]
 
 for k, v in companies.items():
-      url = 'http://www.moneycontrol.com/company-article/{}/news/{}#{}'.format(k, v, v)
-      company = k
-      abbr = v
-      master_list.append({"coy":k, "abbr": v, "url":url})
+    url = 'http://www.moneycontrol.com/company-article/{}/news/{}#{}'.format(k, v, v)
+    company = k
+    abbr = v
+    master_list.append({"coy":k, "abbr": v, "url":url})
 
 print(master_list)
 # Create an empty list which will contain the selected news articles
@@ -29,21 +30,22 @@ List_of_links = []
 
 # Extract the relevant news articles weblinks from the news section of selected companies
 for item in master_list:
-    html = requests.get(item['url'])
-    # Create a BeautifulSoup object
-    soup = BeautifulSoup(html.text, 'html.parser')
-    # Retrieve a list of all the links and the titles for the respective links
-    word1, word2, word3 = "US", "USA", "USFDA"
+   html = requests.get(item['url'])
+   # Create a BeautifulSoup object
+   soup = BeautifulSoup(html.text, 'html.parser')
+
+   # Retrieve a list of all the links and the titles for the respective links
+   word1, word2, word3 = "US", "USA", "USFDA"
 
    sub_links = soup.find_all('a', class_='arial11_summ')
    for links in sub_links:
-        # first convert into a string
-        sp = BeautifulSoup(str(links), 'html.parser')
-        tag = sp.a
-        if word1 in tag['title'] or word2 in tag['title'] or word3 in tag['title']:
-            category_links = Base_url + tag["href"]
-            List_of_links.append({'coy': item['coy'], 'link':category_links})
-            time.sleep(3)
+      # first convert into a string
+      sp = BeautifulSoup(str(links), 'html.parser')
+      tag = sp.a
+      if word1 in tag['title'] or word2 in tag['title'] or word3 in tag['title']:
+          category_links = Base_url + tag["href"]
+          List_of_links.append({'coy': item['coy'], 'link':category_links})
+          time.sleep(3)
 
 # Print the select list of news articles weblinks
 #final_list = list(set(List_of_links))
@@ -69,4 +71,8 @@ for item in List_of_links:
         continue
 
 for entry in data:
-print(entry)
+    print(entry)
+
+
+
+
