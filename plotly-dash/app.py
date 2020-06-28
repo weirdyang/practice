80e4361c56d3fc2fdc547f5f8c9c6b6687d1e639
captmomo <wy4786@gmail.com>
2020-06-28 19:28:46 +0800

    test

--- plotly-dash/app.py
+++ plotly-dash/app.py
@@ -8,6 +8,7 @@ import dash_table
 import pandas as pd
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
+from flask import Response
 
 PARENT_PATH = Path(__file__).parent.resolve()
 SAVE_PATH = os.path.join(PARENT_PATH, "solar.csv")
@@ -25,19 +26,44 @@ def serve_layout():
         editable=True
     ),
     html.Button(id="save-button", n_clicks=0, children="Save"),
-    html.Div(id="notification-div", children="Press button to save changes")])
+    html.Button(id="export-button", n_clicks=0, children="Export to csv"),
+    dcc.Input(id="filename-input", value="export-file", type='text'),
+    html.Div(id="notification-div", children="Press button to save changes"),
+    html.Div(id='placeholder', style={'display': 'none'}),
+    html.Div(id='intermediate-value', style={'display': 'none'})])
 
 
 app.layout = serve_layout
+@app.callback(
+    [Output("data-table", "data"), Output("placeholder", "children")],
+    [Input("export-button", "n_clicks")],
+    [State("data-table", "data"), State("filename-input", "value")])
+def export_data_to_csv(nclicks, data, filename):
+    if nclicks == 0:
+        raise PreventUpdate
+    else:
+        updated = pd.DataFrame(data)
+        updated.to_csv(SAVE_PATH, index=False)
+        return updated.to_dict("rows"), filename
+
+@app.callback(
+    [Output("intermediate-value", "children")],
+    [Input("export-button", "n_clicks")],
+    [State("data-table", "data"), State("filename-input", "value")])
+def generate_csv(filename):
+    updated =  pd.read_csv(SAVE_PATH)
+    return Response(updated.to_csv(index=False),mimetype="text/csv",headers={"Content-disposition": f"attachment; filename={filename}.csv"})
+
 
 @app.callback(
     [Output("data-table", "data"), Output("notification-div", "children")],
-    [Input("save-button", "n_clicks")],
+    [Input("save-button", "n_clicks"),Input("export-button", "n_clicks")],
     [State("data-table", "data")])
 def selected_data_to_csv(nclicks, data):
     if nclicks == 0:
         raise PreventUpdate
     else:
+        ctx = dash.callback_context
         updated = pd.DataFrame(data)
         updated.to_csv(SAVE_PATH, index=False)
         return updated.to_dict("rows"), f"Data updated and saved to {SAVE_PATH}"
